ed301b5218520d206a0e3327882a8df3270a9cf5
keke <keke@ky-tech.com.cn>
2023-04-04 22:41:04 +0800

    fix

--- image-preview/src/main/java/com/ky/android/photo/VideoFragment.kt
+++ image-preview/src/main/java/com/ky/android/photo/VideoFragment.kt
@@ -1,23 +1,24 @@
 package com.ky.android.photo
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.danikula.videocache.HttpProxyCacheServer
 import com.ky.android.photo.config.ContentViewOriginModel
 import com.ky.android.photo.databinding.FragmentVideoBinding
 import xyz.doikki.videocontroller.StandardVideoController
 
-class VideoFragment : Fragment() {
+open class VideoFragment : Fragment() {
 
     private lateinit var _binding: FragmentVideoBinding
 
     private var url: String? = null
     private var position: Int? = null
     private var originModel: ContentViewOriginModel? = null
+    private var isLoad = false;
 
     companion object {
         fun newInstance() = VideoFragment()
@@ -40,8 +41,10 @@ class VideoFragment : Fragment() {
         val controller = context?.let { StandardVideoController(it) }
         controller?.addDefaultControlComponent("标题", false)
         _binding.player.setVideoController(controller) //设置控制器
-        _binding.player.start() //开始播放，不调用则不自动播放
+    }
 
+    private fun isVisibleToUser(): Boolean {
+        return isResumed && userVisibleHint
     }
 
     private fun initIntent() {
@@ -53,17 +56,28 @@ class VideoFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this)[VideoViewModel::class.java]
-
     }
 
     override fun onPause() {
         super.onPause()
-        _binding.player.pause()
+        if (userVisibleHint) {
+            onUserVisibleChanged(false)
+        }
     }
 
     override fun onResume() {
         super.onResume()
-        _binding.player.resume()
+        if (userVisibleHint) {
+            onUserVisibleChanged(true)
+        }
+    }
+
+
+    override fun setUserVisibleHint(isVisibleToUser: Boolean) {
+        super.setUserVisibleHint(isVisibleToUser)
+        if (isResumed) {
+            onUserVisibleChanged(isVisibleToUser)
+        }
     }
 
     override fun onDestroy() {
@@ -71,4 +85,18 @@ class VideoFragment : Fragment() {
         _binding.player.release()
     }
 
+
+    private fun onUserVisibleChanged(isVisibleToUser: Boolean) {
+        // 不可见的时候暂停分块显示器，节省内存，可见的时候恢复
+        if (isVisibleToUser) {
+            if (!isLoad && isVisibleToUser()) {
+                isLoad = true
+                _binding.player.start()
+            } else {
+                _binding.player.resume()
+            }
+        } else {
+            _binding.player.pause()
+        }
+    }
 }
